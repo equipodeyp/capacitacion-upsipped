88748543e8b33731c4cc391da8f6460731a81519
santijhon <santiagojhon037@gmail.com>
2025-01-24 12:13:49 -0600

    actualizacion de tabla menu adminstrador

--- administrador/admin.php
+++ administrador/admin.php
@@ -13,48 +13,39 @@ $row=$result->fetch_assoc();
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-  <title>CAPACITACION UPSIPPED</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/bootstrap-theme.css" rel="stylesheet">
-  <!-- <script src="../js/bootstrap.min.js"></script> -->
-  <link rel="stylesheet" href="../css/cli.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/solid.css" integrity="sha384-DhmF1FmzR9+RBLmbsAts3Sp+i6cZMWQwNTRsew7pO/e4gvzqmzcpAzhDIwllPonQ" crossorigin="anonymous"/>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/fontawesome.css" integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous"/>
-  <link href="../css/bootstrap.min.css" rel="stylesheet">
-  <link href="../css/bootstrap-theme.css" rel="stylesheet">
-  <script src="../js/jquery-3.1.1.min.js"></script>
-  <link href="../css/jquery.dataTables.min.css" rel="stylesheet">
-  <script src="../js/jquery.dataTables.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="../css/breadcrumb.css">
-  <link rel="stylesheet" href="../css/expediente.css">
-  <link rel="stylesheet" href="../css/font-awesome.css">
-  <link rel="stylesheet" href="../css/cli.css">
-  <script src="../js/expediente.js"></script>
-  <script src="../js/solicitud.js"></script>
-  <link rel="stylesheet" href="../css/cli.css">
-  <link rel="stylesheet" href="../css/registrosolicitud1.css">
-  <script src="../js/Javascript.js"></script>
-  <!-- <script src="../js/validar_campos.js"></script> -->
-  <script src="../js/verificar_camposm1.js"></script>
-  <script src="../js/mascara2campos.js"></script>
-  <script src="../js/mod_medida.js"></script>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/solid.css" integrity="sha384-DhmF1FmzR9+RBLmbsAts3Sp+i6cZMWQwNTRsew7pO/e4gvzqmzcpAzhDIwllPonQ" crossorigin="anonymous"/>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/fontawesome.css" integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous"/>
-  <link rel="stylesheet" href="../css/main2.css">
+  <script src="../js/botonatras.js"></script>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>CAPACITACION SIPPSIPPED</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="../js/jquery-3.1.1.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<link href="../css/bootstrap-theme.css" rel="stylesheet">
+<script src="../js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="../css/cli.css">
+<!-- CSS personalizado -->
+<link rel="stylesheet" href="../css/main2.css">
+<!--datables CSS básico-->
+<link rel="stylesheet" type="text/css" href="../datatables/datatables.min.css"/>
+<!--datables estilo bootstrap 4 CSS-->
+<link rel="stylesheet"  type="text/css" href="../datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+<!--font awesome con CDN-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<!-- datatables JS -->
+<script type="text/javascript" src="../datatables/datatables.min.js"></script>
+<!-- para usar botones en datatables JS -->
+<script src="../datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
+<script src="../datatables/JSZip-2.5.0/jszip.min.js"></script>
+<script src="../datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
+<script src="../datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
+<script src="../datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/solid.css" integrity="sha384-DhmF1FmzR9+RBLmbsAts3Sp+i6cZMWQwNTRsew7pO/e4gvzqmzcpAzhDIwllPonQ" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/fontawesome.css" integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous"/>
+<!-- MATERIAL PARA USAR TOAST MENSAJES DE ALERTA  -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
 <!-- SCRIPT PARA EL MANEJO DE LA TABLA -->
-  <!-- <script type="text/javascript">
+  <script type="text/javascript">
   $(document).ready(function() {
       $('#example').DataTable({
           language: {
@@ -72,137 +63,10 @@ $row=$result->fetch_assoc();
              },
              "sProcessing":"Procesando...",
               },
-          // para usar los botones
-          responsive: "true",
-          dom: 'Bfrtilp',
-          buttons:[
-        {
-          extend:    'excelHtml5',
-          text:      '<i class="fas fa-file-excel"></i> ',
-          titleAttr: 'Exportar a Excel',
-          className: 'btn color-btn-export-xls'
-        },
-        {
-          extend:    'pdfHtml5',
-          text:      '<i class="fas fa-file-pdf"></i> ',
-          titleAttr: 'Exportar a PDF',
-          className: 'btn color-btn-export-ppt'
-        },
-        {
-          extend:    'print',
-          text:      '<i class="fa fa-print"></i> ',
-          titleAttr: 'Imprimir',
-          className: 'btn color-btn-export-imp'
-        },
-      ]
       });
   });
-  </script> -->
+  </script>
 
-  <style>
-    .pagination {
-  display: inline-block;
-  padding-left: 0;
-  margin: 20px 0;
-  border-radius: 4px;
-}
-.pagination > li {
-  display: inline;
-}
-.pagination > li > a,
-.pagination > li > span {
-  position: relative;
-  float: left;
-  padding: 6px 12px;
-  margin-left: -1px;
-  line-height: 1.42857143;
-  color: #63696D;
-  text-decoration: none;
-  background-color: #fff;
-  border: 1px solid #ddd;
-}
-.pagination > li:first-child > a,
-.pagination > li:first-child > span {
-  margin-left: 0;
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-.pagination > li:last-child > a,
-.pagination > li:last-child > span {
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
-}
-.pagination > li > a:hover,
-.pagination > li > span:hover,
-.pagination > li > a:focus,
-.pagination > li > span:focus {
-  z-index: 2;
-  color: #63696D;
-  background-color: #eee;
-  border-color: #ddd;
-}
-.pagination > .active > a,
-.pagination > .active > span,
-.pagination > .active > a:hover,
-.pagination > .active > span:hover,
-.pagination > .active > a:focus,
-.pagination > .active > span:focus {
-  z-index: 3;
-  color: #fff;
-  cursor: default;
-  background-color: #63696D;
-  border-color: #5F6D6B;
-}
-.pagination > .disabled > span,
-.pagination > .disabled > span:hover,
-.pagination > .disabled > span:focus,
-.pagination > .disabled > a,
-.pagination > .disabled > a:hover,
-.pagination > .disabled > a:focus {
-  color: #777;
-  cursor: not-allowed;
-  background-color: #fff;
-  border-color: #ddd;
-}
-.pagination-lg > li > a,
-.pagination-lg > li > span {
-  padding: 10px 16px;
-  font-size: 18px;
-  line-height: 1.3333333;
-}
-.pagination-lg > li:first-child > a,
-.pagination-lg > li:first-child > span {
-  border-top-left-radius: 6px;
-  border-bottom-left-radius: 6px;
-}
-.pagination-lg > li:last-child > a,
-.pagination-lg > li:last-child > span {
-  border-top-right-radius: 6px;
-  border-bottom-right-radius: 6px;
-}
-.pagination-sm > li > a,
-.pagination-sm > li > span {
-  padding: 5px 10px;
-  font-size: 12px;
-  line-height: 1.5;
-}
-.pagination-sm > li:first-child > a,
-.pagination-sm > li:first-child > span {
-  border-top-left-radius: 3px;
-  border-bottom-left-radius: 3px;
-}
-.pagination-sm > li:last-child > a,
-.pagination-sm > li:last-child > span {
-  border-top-right-radius: 3px;
-  border-bottom-right-radius: 3px;
-}
-
-a:hover,
-a:focus {
-  color: #FFFFFF;
-  text-decoration: underline;
-}
-  </style>
 </head>
 <body>
   <div class="contenedor">
@@ -226,10 +90,8 @@ a:focus {
         ?>
         <h6 style="text-align:center" class='user-nombre'>  <?php echo "" . $_SESSION['usuario']; ?> </h6>
       </div>
-
       <nav class="menu-nav">
         <ul>
-
             <li class="menu-items"><a  href="#" onclick="location.href='registrarservidor.php'"><i class="color-icon fa-solid fa-comment-dots menu-nav--icon fa-fw"></i><span style="color: white; font-weight:bold;">SERVIDOR PUBLICO</span></a></li>
             <li class="menu-items"><a  href="#" onclick="location.href='registrarcapacitacion.php'"><i class="color-icon fa-solid fa-comment-dots menu-nav--icon fa-fw"></i><span style="color: white; font-weight:bold;">CAPACITACIÓN</span></a></li>
             <li class="menu-items"><a  href="#" onclick="location.href='asignarcurso.php'"><i class="color-icon fa-solid fa-comment-dots menu-nav--icon fa-fw"></i><span style="color: white; font-weight:bold;">ASIGNAR CURSO</span></a></li>
@@ -241,55 +103,36 @@ a:focus {
     </nav>
 
     </div>
+    <!-- checar -->
     <div class="main bg-light">
       <div class="barra">
           <img src="../image/fiscalia.png" alt="" width="150" height="150">
           <img src="../image/capupsipped.png" alt="" width="1080" height="170">
       </div>
-        <br><br><br><br><br><br><br>
-        <!-- <div class="row">
-          <h1 style="text-align:center">
-            <?php echo mb_strtoupper (html_entity_decode($row['nombre'], ENT_QUOTES | ENT_HTML401, "UTF-8")); ?> </span>
-            <?php echo mb_strtoupper (html_entity_decode($row['apellido_p'], ENT_QUOTES | ENT_HTML401, "UTF-8")); ?> </span>
-            <?php echo mb_strtoupper (html_entity_decode($row['apellido_m'], ENT_QUOTES | ENT_HTML401, "UTF-8")); ?> </span>
-          </h1> -->
-          <h5 style="text-align:center">
-            <!-- <?php echo utf8_decode(strtoupper($row['area'])); ?> </span> -->
-
-
-
-                  <div class="">
-                        <div class="row">
-                                <div class="col-lg-12">
-
-
-                                    <div class="table-responsive">
-                                        <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                                        <thead>
-                                          <h3 style="text-align:center">Registros</h3>
-                                            <tr>
-                                                <th style="text-align:center">No.</th>
-                                                <th style="text-align:center">NOMBRE DE LA CAPACITACION</th>
-                                                <th style="text-align:center">FECHA DE INICIO</th>
-                                                <th style="text-align:center">FECHA DE TERMINO</th>
-                                                <th style="text-align:center">NO. DE PARTICIPANTES</th>
-                                                <th style="text-align:center">LISTA DE PARTICIPANTES</th>
-
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                          <?php
+      <div class="">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="table-responsive">
+              <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                  <h3 style="text-align:center">Registros</h3>
+                  <tr>
+                    <th style="text-align:center">No.</th>
+                    <th style="text-align:center">NOMBRE DE LA CAPACITACION</th>
+                    <th style="text-align:center">FECHA DE INICIO</th>
+                    <th style="text-align:center">FECHA DE TERMINO</th>
+                    <th style="text-align:center">NO. DE PARTICIPANTES</th>
+                    <th style="text-align:center">LISTA DE PARTICIPANTES</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
                                           $contador= 0;
-
-
                                           $tabla="SELECT * FROM datos_capacitaciones";
                                           $var_resultado = $mysqli->query($tabla);
-
                                           while ($var_fila=$var_resultado->fetch_array())
                                           {
                                             $fol_exp2=$var_fila['id'];
-
-
                                             $abc="SELECT count(*) as c FROM datos_capacitaciones";
                                             $result=$mysqli->query($abc);
                                             if($result)
@@ -309,7 +152,7 @@ a:focus {
                                                 echo "<a href='#edit_".$var_fila['id']."' class='btn color-btn-success btn-sm' data-toggle='modal'><i class='fa-solid fa-file-pen'></i> Detalle</a>";
                                                 include('verparticipantes.php');
                                                 echo "</td>";
-
+                                                echo "</tr>";
 
                                               }
 
@@ -318,14 +161,14 @@ a:focus {
 
                                         ?>
 
-                                        </tbody>
-                                       </table>
-                                    </div>
-                                </div>
-                        </div>
-
-    <a href="../logout.php" class="btn-flotante-dos">Cerrar Sesión</a>
-  </div>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <a href="../logout.php" class="btn-flotante-dos">Cerrar Sesión</a>
+        </div>
+      </div>
   <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
   <!-- <script src="js/jquery.min.js"></script> -->
   <!-- <script src="bootstrap/js/bootstrap.min.js"></script> -->
